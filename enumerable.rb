6bc6d442189836f043da8ca327bc3817fa8cb00f
Lordkaito <cespedesisai88@gmail.com>
2021-12-10 15:00:49 +0100

    Add enumerable.rb file

---
+++ enumerable.rb
@@ -0,0 +1,18 @@
+module MyEnumerable
+
+  def all?
+    each { |item| return false unless yield(item) }
+    true
+  end
+
+  def any?
+    each { |item| return true if yield(item) }
+    false
+  end
+
+  def filter?
+    result = []
+    each { |item| result << element if yield item }
+    result
+  end
+end
